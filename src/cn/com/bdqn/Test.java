52ef19b570776beed633a6852ce1a972a52d8f63
dingwanga <m15717222852@163.com>
2019-02-23 09:31:05 +0800

    update Test.java04

--- src/cn/com/bdqn/Test.java
+++ src/cn/com/bdqn/Test.java
@@ -7,6 +7,7 @@ public class Test {
 		System.out.println("hello,git");
 		System.out.println("hello,git服务器修改");
 		System.out.println("hello,git服务器修改02");
+		System.out.println("Eclipse上的修改！");
 	}
 
 }
