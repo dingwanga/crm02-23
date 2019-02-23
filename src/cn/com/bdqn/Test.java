a6b8a7612cbbf1fd17c69f546803b00c042945e8
dingw <36919366+dingwanga@users.noreply.github.com>
2019-02-23 11:35:33 +0800

    update file
    222222

--- src/cn/com/bdqn/Test.java
+++ src/cn/com/bdqn/Test.java
@@ -8,6 +8,7 @@ public class Test {
 		System.out.println("hello,git服务器修改");
 		System.out.println("hello,git服务器修改02");
 		System.out.println("Eclipse上的修改！");
+		System.out.println("服务器上的修改！");
 	}
 
 }
